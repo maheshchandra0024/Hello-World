2e865b98f215f80a9034de38f0054bdac93b375d
Mahesh <maheshchandra.neelam@valpo.edu>
2019-01-27 22:38:15 -0500

    Update views.py

--- First_Django/Blog/views.py
+++ First_Django/Blog/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 hey = [
 	{
-		"Year" : "2004",
-		"Make" : "HONDA COUPE",
-		"Model": "ACCORD"
+		"Year" : "2008",
+		"Make" : "KIA",
+		"Model": "SPECTRA EX"
 	}
 ]
 def Home(request):
