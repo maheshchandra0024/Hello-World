17340b9569417e029ddf5ed27072398f146f2721
Mahesh <maheshchandra.neelam@valpo.edu>
2019-04-04 11:38:04 -0400

    plzz wrk

--- First_Django/Blog/views.py
+++ First_Django/Blog/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 hey = [
 	{
-		"Year": "2222",
-		"Make": "CHEVROLET",
-		"Model": "PIGLET"
+		"Year": "2019",
+		"Make": "CHEVY",
+		"Model": "MALIBU"
 	}
 ]
 
